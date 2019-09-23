c407be8073c788b08290ec9cc202d0b385ac340e
Martin Vivanco <martinvivanco@MacBook-Pro-de-POSGRADO.local>
2019-09-23 11:56:55 -0500

    add coments and format

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,11 @@
 class SudokuTable:
+    """class sudoku table that will help
+    us modelling the game board and its rules"""
     def __init__(self):
         self.chart = [[0]*9]*9
 
     def print_table(self):
+        """prints the game board with a format"""
         row = 0
         col = 0
         for i in range(9):
@@ -21,18 +24,21 @@ class SudokuTable:
         print '\n'
 
     def used_in_row(self, row, num):
+        """checks if the given number exists in the given row"""
         for col in range(9):
             if self.chart[row][col] == num:
                 return True
         return False
 
     def used_in_col(self, col, num):
+        """checks if the given number exists in the given col"""
         for row in range(9):
             if self.chart[row][col] == num:
                 return True
         return False
 
     def used_in_box(self, colstart, rowstart, num):
+        """checks if the given number exists in the given 3x3 box"""
         for row in range(3):
             for col in range(3):
                 if self.chart[row + rowstart][col + colstart] == num:
@@ -40,19 +46,22 @@ class SudokuTable:
         return False
 
     def can_place(self, col, row, num):
+        """checks that all the constraints are passed"""
         a = self.used_in_col(row, num)
         b = self.used_in_row(col, num)
         c = self.used_in_box(row - row % 3, col - col % 3, num)
         return  (not a) and (not b) and (not c)
 
     def get_empty_loc(self):
+        """checks if chart is complete and return incomplete locations"""
         for row in range(9):
             for col in range(9):
                 if self.chart[row][col] == 0:
                     return (row, col)
         return (-1,-1)
 
-def solve_soduko(sudoku):
+def solve_soduku(sudoku):
+    """DFS to solve the sudoku recursively"""
     (row, col) = sudoku.get_empty_loc()
     #sudoku.print_table()
     if (row == -1) and (col == -1):
@@ -61,7 +70,7 @@ def solve_soduko(sudoku):
     for num in range(1,10):
         if sudoku.can_place(row, col, num):
         	sudoku.chart[row][col] = num
-        	if solve_soduko(sudoku):
+        	if solve_soduku(sudoku):
         		return True
         	sudoku.chart[row][col] = 0
 
@@ -78,5 +87,12 @@ sudoku.chart = [
     [0,0,1,0,0,0,0,6,8],
     [0,0,8,5,0,0,0,1,0],
     [0,9,0,0,0,0,4,0,0]]
-if solve_soduko(sudoku):
+print '*----------Sudoku Solver------------*\n'
+print '*----------Inicial Sudoku------------*'
+sudoku.print_table()
+if solve_soduku(sudoku):
+    print '\n*----------Solved Sudoku------------*'
     sudoku.print_table()
+else:
+    print '\n*----------Solved Sudoku------------*'
+    print '* cant solve this sudoku *'
