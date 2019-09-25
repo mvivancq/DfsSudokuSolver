82000d9f7c7e25cef9036c9e4dcb3df680bd0044
Martin Vivanco <martinvivanco@MacBook-Pro-de-POSGRADO.local>
2019-09-25 14:16:23 -0500

    add n numbers

--- sudokunnumbers.py
+++ sudokunnumbers.py
@@ -1,20 +1,22 @@
+with_heigth = 4
+box_size = 2
 class SudokuTable:
     """class sudoku table that will help
     us modelling the game board and its rules"""
     def __init__(self):
-        self.chart = [[0]*9]*9
+        self.chart = [[0]*with_heigth]*with_heigth
 
     def print_table(self):
         """prints the game board with a format"""
         row = 0
         col = 0
-        for i in range(9):
-            for j in range(9):
-                if col == 3:
+        for i in range(with_heigth):
+            for j in range(with_heigth):
+                if col == box_size:
                     print '|',
                     col = 0
-                if row == 3:
-                    print '----------------------'
+                if row == box_size:
+                    print '-------------------'
                     row = 0
                 print self.chart[i][j],
                 col = col + 1
@@ -25,22 +27,22 @@ class SudokuTable:
 
     def used_in_row(self, row, num):
         """checks if the given number exists in the given row"""
-        for col in range(9):
+        for col in range(with_heigth):
             if self.chart[row][col] == num:
                 return True
         return False
 
     def used_in_col(self, col, num):
         """checks if the given number exists in the given col"""
-        for row in range(9):
+        for row in range(with_heigth):
             if self.chart[row][col] == num:
                 return True
         return False
 
     def used_in_box(self, colstart, rowstart, num):
         """checks if the given number exists in the given 3x3 box"""
-        for row in range(3):
-            for col in range(3):
+        for row in range(box_size):
+            for col in range(box_size):
                 if self.chart[row + rowstart][col + colstart] == num:
                     return True
         return False
@@ -49,13 +51,13 @@ class SudokuTable:
         """checks that all the constraints are passed"""
         a = self.used_in_col(row, num)
         b = self.used_in_row(col, num)
-        c = self.used_in_box(row - row % 3, col - col % 3, num)
+        c = self.used_in_box(row - row % box_size, col - col % box_size, num)
         return  (not a) and (not b) and (not c)
 
     def get_empty_loc(self):
         """checks if chart is complete and return incomplete locations"""
-        for row in range(9):
-            for col in range(9):
+        for row in range(with_heigth):
+            for col in range(with_heigth):
                 if self.chart[row][col] == 0:
                     return (row, col)
         return (-1,-1)
@@ -67,7 +69,7 @@ def solve_soduku(sudoku):
     if (row == -1) and (col == -1):
         return True
 
-    for num in range(1,10):
+    for num in range(1,with_heigth+1):
         if sudoku.can_place(row, col, num):
         	sudoku.chart[row][col] = num
         	if solve_soduku(sudoku):
@@ -78,15 +80,10 @@ def solve_soduku(sudoku):
 
 sudoku = SudokuTable()
 sudoku.chart = [
-    [8,0,0,0,0,0,0,0,0],
-    [0,0,3,6,0,0,0,0,0],
-    [0,7,0,0,9,0,2,0,0],
-    [0,5,0,0,0,7,0,0,0],
-    [0,0,0,0,4,5,7,0,0],
-    [0,0,0,1,0,0,0,3,0],
-    [0,0,1,0,0,0,0,6,8],
-    [0,0,8,5,0,0,0,1,0],
-    [0,9,0,0,0,0,4,0,0]]
+    [3,4,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,1,3,0]]
 print '*----------Sudoku Solver------------*\n'
 print '*----------Inicial Sudoku------------*'
 sudoku.print_table()
